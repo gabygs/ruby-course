c568de56a656c916c3d59126f810b19f848a5749
profgs <ggongora@cmu.edu>
2021-01-27 23:03:52 -0500

    added comments

--- montys-color.rb
+++ montys-color.rb
@@ -18,3 +18,15 @@ if color == "blue"
 else
 	puts "I'm not a big fan of #{color}."
 end
+
+#Congratulations! You've written and ran a Ruby program 
+#that not only prints strings, but takes user input and 
+#prints one of two separate responses depending on that input. 
+
+#Here are some suggestions for things you can do to expand 
+#on what we learned here today:
+
+#Look into Elsif Statements to create a third branching pathway. For example, if the user inputs "red," make the program respond with something like "It's not as good as blue, but red isn't too bad."
+#Look into the downcase method and implement it into the project so that no matter what case the user inputs "blue" in, they'll see the expected response.
+#Look into Logical Operators to make the program respond with their favorite color being either "blue" or "green."
+
