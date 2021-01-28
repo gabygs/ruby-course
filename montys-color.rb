a14097bc41592ce4fb3a4956695e0789e886b395
profgs <ggongora@cmu.edu>
2021-01-27 23:02:11 -0500

    add else statement

--- montys-color.rb
+++ montys-color.rb
@@ -7,14 +7,14 @@ color = gets.chop
 # Lesson 4: Using an If Statement
 # Ruby is that it prioritizes readability 
 #because it's written like plain English
-if color == "blue"
-	puts "Wow! My favorite color is #{color} also!"
-end
+#if color == "blue"
+#	puts "Wow! My favorite color is #{color} also!"
+#end
 
 # Lesson 5: Adding an Else Statement
 
-# if color == "blue"
-#     puts "Wow! My favorite color is #{color} also!"
-# else
-#     puts "I'm not a big fan of #{color}."
-# end
+if color == "blue"
+	puts "Wow! My favorite color is #{color} also!"
+else
+	puts "I'm not a big fan of #{color}."
+end
