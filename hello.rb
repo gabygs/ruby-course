06d5b7a9f49d251b5d8810b570de2ce4a36ef6e9
profgs <ggongora@cmu.edu>
2021-01-27 22:55:43 -0500

    add ruby methods

--- hello.rb
+++ hello.rb
@@ -1,8 +1,21 @@
 # Lesson 1: Outputting a String
 
-puts "Hello World!"
+#puts "Hello World!"
 
 # Lesson 2: Prompting User Input
 
-# name = gets
-# puts "Hey, #{name}! Nice to meet you!"
+#name = gets
+#this introduces a string interpolation
+#string interpolation is used to take the content 
+#within a variable and insert it into a string
+#gets method captures all keystrokes including the ENTER key
+# we can fix this with the method chop on captured string 
+#puts "Hey, #{name}! Nice to meet you!"
+
+#chop takes a string and removes the very last character of the string
+#in this case a newline
+
+puts "What is your name?"
+name = gets.chop
+puts "Hey, #{name}! Nice to meet you!"
+
